17063cce315b7aefaee22a411454237c50250a0b
mikeiz404 <computergeekxp@gmail.com>
2014-02-25 15:45:00 -0800

    Init with scaffolding. Cleaned up and added foundation css.

---
+++ project/__init__.py
@@ -0,0 +1,31 @@
+from pyramid.config import Configurator
+from pyramid.events import NewRequest
+from pymongo import MongoClient
+
+from project.resources import Root
+
+def main(global_config, **settings):
+    """
+    This function returns a WSGI application.
+    """
+    config = Configurator(settings=settings, root_factory=Root)
+    # set static dir
+    config.add_static_view('static', 'project:static')
+    # set templating
+    config.include('pyramid_chameleon')
+    # setup mongodb
+    db_uri = settings['mongodb.url']
+    dbc = MongoClient(db_uri)
+    config.registry.settings['mongodb_conn'] = dbc
+    def add_mongo_db(event):
+        settings = event.request.registry.settings
+        url = settings['mongodb.url']
+        db_name = settings['mongodb.db_name']
+        db_conn = settings['mongodb_conn']
+        db = db_conn[db_name]
+        event.request.db_conn = db_conn
+        event.request.db = db
+    config.add_subscriber(add_mongo_db, NewRequest)
+    # scan project
+    config.scan('project')
+    return config.make_wsgi_app()
