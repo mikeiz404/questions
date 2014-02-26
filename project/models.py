3bc0a8646a8105cfed457d190a41eec31c765e9d
mikeiz404 <computergeekxp@gmail.com>
2014-02-26 11:57:32 -0800

    Refactored SyncedDocument into ElasticSyncedDocument and MongoDocument.

--- project/models.py
+++ project/models.py
@@ -2,18 +2,16 @@ from elasticsearch import Elasticsearch
 from pymongo import MongoClient
 from bson import ObjectId
 
-class SyncedDocument(dict):
+class MongoDocument(dict):
     """
-    A bare bones implementation of an elastic search synced mongo document.
+    A bare bones implementation of a mongo document.
     TODO: implement better error handling
     TODO: use a connection pool instead of class based connections
     TODO: add tests
     """
     _id = None
-    elastic = Elasticsearch()
-    mongodb = MongoClient()
-    _mongo = {'db': None, 'collection': None}
-    _elastic = {'index': None, 'type': None}
+    _mongodb = MongoClient()
+    mongo_config = {}
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
@@ -28,53 +26,74 @@ class SyncedDocument(dict):
             data['_id'] = ObjectId(self._id)
         return data
 
-    def to_elastic(self):
-        return dict(self)
-
-    def save_elastic(self):
-        assert(self._id is not None)
-        index = self._elastic.get('index')
-        type = self._elastic.get('type')
-        self.elastic.index(index, type, id=self._id, body=self.to_elastic())
-
-    def save_mongo(self):
-        db = self._mongo.get('db')
-        collection = self._mongo.get('collection')
-        object_id = self.mongodb[db][collection].save(self.to_mongo())
+    def save(self):
+        db = self.mongo_config.get('db')
+        collection = self.mongo_config.get('collection')
+        object_id = self._mongodb[db][collection].save(self.to_mongo())
         # update id
         self._id = str(object_id)
 
-    def save(self):
-        self.save_mongo()
-        self.save_elastic()
-
-    def delete_elastic(self):
+    def delete(self):
         if self._id is not None:
-            index = self._elastic.get('index')
-            type = self._elastic.get('type')
-            self.elastic.delete(index, type, self._id)
-        else:
-            raise 'Object _id is None.'
-
-    def delete_mongo(self):
-        if self._id is not None:
-            db = self._mongo.get('db')
-            collection = self._mongo.get('collection')
-            self.mongodb[db][collection].remove({"_id": ObjectId(self._id)})
+            db = self.mongo_config.get('db')
+            collection = self.mongo_config.get('collection')
+            self._mongodb[db][collection].remove({"_id": ObjectId(self._id)})
             # update id
             self._id = None
         else:
             raise 'Object _id is None.'
 
+    @classmethod
+    def search(clazz, *args, **kwargs):
+        db = clazz.mongo_config.get('db')
+        collection = clazz.mongo_config.get('collection')
+        results = clazz._mongodb[db][collection].find(*args, **kwargs)
+        for result in results:
+            object = clazz(**result)
+            yield object
+
+
+class ElasticSyncedDocument(MongoDocument):
+    """
+    A bare bones implementation of an elastic search synced mongo document.
+    TODO: implement better error handling
+    TODO: use a connection pool instead of class based connections
+    TODO: add tests
+    """
+    _elastic = Elasticsearch()
+    elastic_config = {'index': None, 'type': None}
+
+    def to_elastic(self):
+        return dict(self)
+
+    def save_elastic(self):
+        assert(self._id is not None)
+        index = self.elastic_config.get('index')
+        type = self.elastic_config.get('type')
+        self._elastic.index(index, type, id=self._id, body=self.to_elastic())
+
+    def save(self):
+        super(ElasticSyncedDocument, self).save()
+        self.save_elastic()
+
+    def delete_elastic(self):
+        if self._id is not None:
+            index = self.elastic_config.get('index')
+            type = self.elastic_config.get('type')
+            self._elastic.delete(index, type, self._id)
+        else:
+            raise 'Object _id is None.'
+
+
     def delete(self):
         self.delete_elastic()
-        self.delete_mongo()
+        super(ElasticSyncedDocument, self).delete()
 
     @classmethod
     def search_elastic(clazz, body):
-        index = clazz._elastic.get('index')
-        type = clazz._elastic.get('type')
-        results = clazz.elastic.search(index, type, body)
+        index = clazz.elastic_config.get('index')
+        type = clazz.elastic_config.get('type')
+        results = clazz._elastic.search(index, type, body)
         # reconstitute
         for data in results['hits']['hits']:
             object = clazz(_id=data['_id'], **(data['_source']))
@@ -82,13 +101,8 @@ class SyncedDocument(dict):
 
     @classmethod
     def search_mongo(clazz, *args, **kwargs):
-        db = clazz._mongo.get('db')
-        collection = clazz._mongo.get('collection')
-        results = clazz.mongodb[db][collection].find(*args, **kwargs)
-        for result in results:
-            object = clazz(**result)
-            yield object
+        return super(ElasticSyncedDocument, clazz).search(*args, **kwargs)
 
-class Question(SyncedDocument):
-    _mongo = {'db': 'project', 'collection': 'questions'}
-    _elastic = {'index': 'project', 'type': 'questions'}
+class Question(ElasticSyncedDocument):
+    mongo_config = {'db': 'project', 'collection': 'questions'}
+    elastic_config = {'index': 'project', 'type': 'questions'}
